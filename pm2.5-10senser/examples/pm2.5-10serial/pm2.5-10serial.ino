ba31afa3689c32f9ebbe3690f778452f4dbedd08
vyuke <vyuke@yuke-MacBook-Pro.local>
2017-04-13 16:00:26 +0800

    arduinoEX

---
+++ pm2.5-10senser/examples/pm2.5-10serial/pm2.5-10serial.ino
@@ -0,0 +1,25 @@
+#include "SDS021sensor.h"
+
+pm2510senser PM1;
+
+void setup() 
+{ 
+   Serial1.begin(9600);
+   Serial.begin(9600);
+   delay(1000);
+}
+
+void loop() 
+{  //Output PM2.5 value
+   float  pm25_val = PM1.Getpm2_5index();
+   Serial.print(pm25_val);
+   Serial.println(" ug/m^3");
+   delay(1000);
+   
+   //Output PM10 value
+//    float  pm10val = PM1.Getpm10index();
+//    Serial.print(pm10val);
+//    Serial.println(" ug/m^3");
+//    delay(1000);
+   
+}
